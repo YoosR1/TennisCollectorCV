d56da85ffb630ab85fc4388e5847d5841f1a8254
Jorge Riera <joluriera@gmail.com>
2023-11-27 01:50:29 -0500

    GPIO interrupt skeleton

--- mainloop.py
+++ mainloop.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from picamera2 import Picamera2
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # Double check for installation of package on RasPi
 
 from BallDetect import *
 from LineDetect import *
+
+# Command is value from 0 to 11, corresponding to the 6 commands of the
+# motor controller, 0-5 being low priority, 6-11 being high priority
+# For reference, motor controller commands are
+# {fullLeft, slightLeft, straight, slightRight, fullRight, stop}
+command = -1
 
 inputGPIO = 18 # physical pin 12
 outputGPIO = 17 # physical pin 11
@@ -12,22 +18,23 @@ outputGPIO = 17 # physical pin 11
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(inputGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(outputGPIO, GPIO.OUT, initial=GPIO.LOW)
+# TODO: figure out how to transmit value
+def commandRequest():
+    # GPIO.output(outputGPIO, GPIO.HIGH)
+    return command
+
+GPIO.add_event_detect(inputGPIO, GPIO.RISING, callback=commandRequest, bouncetime=100)
 
 # refimage should be of 2in court line 4ft away
 # might use image from 2ft away
 refImage = mpimg.imread('testimage/2in2ft.jpg')
-focalLength = findFocalLength(refImage, 2, 48)
+focalLength = findFocalLength(refImage, 2, 24)
 
 cam = Picamera2()
 # TODO: test if lower resolution helps with capture/processing time
 config = cam.create_still_configuration(main={'size': (1152, 648)})
 cam.configure(config)
 
-# Command is value from 0 to 11, corresponding to the 6 commands of the
-# motor controller, 0-5 being low priority, 6-11 being high priority
-# For reference, motor controller commands are
-# {fullLeft, slightLeft, straight, slightRight, fullRight, stop}
-command = -1
 while True:
     cam.start()
     currImage = cam.capture_array()
@@ -44,8 +51,6 @@ while True:
             ballDir[ball] += 1
         command = max(ballDir)
 
-def commandRequest():
-    return command
 
 # TEST CODE
 # try:
